64f3b069b44be03228fa696bf847400d1751f1b9
Oscar Beaumont <oscar@otbeaumont.me>
2021-12-18 00:58:31 +0800

    fix: corrected handling of nullable column

--- src/seaorm_integration.rs
+++ src/seaorm_integration.rs
@@ -19,7 +19,7 @@ pub(crate) fn get_column_def<T: EntityTrait>(column: T::Column) -> ColumnDef {
     let column_def_prelude: CustomColumnDef = unsafe { std::mem::transmute(column.def()) }; // Note: This is used to access private fields and hence relies on internal implementation details of sea_query and unsafe code which is not good!
     let mut column_def =
         ColumnDef::new_with_type(column, column_def_prelude.col_type.clone().into());
-    if column_def_prelude.null {
+    if !column_def_prelude.null {
         column_def.not_null();
     }
     if column_def_prelude.unique {
